dc687cda1c5996b69ffc9f7d97d91ada010185b3
Luxian <lux1an@qq.com>
2026-07-26 18:58:12 +0800

    chore(orion): upgrade scorpiofs FUSE stack

--- orion/src/main.rs
+++ orion/src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::new("info,rfuse3::raw::session=error")),
+                .unwrap_or_else(|_| EnvFilter::new("info,asyncfuse::raw::session=error")),
         )
         .with_target(true)
         .with_writer(|| LineWriter::new(stderr()))
